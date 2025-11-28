74132fff8dbfbd4216ad1ed2a2d0760500baa8f2
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-11-28 17:40:16 +0500

    working on backend app

--- appBackend/src/config/prisma.ts
+++ appBackend/src/config/prisma.ts
@@ -1,3 +1,5 @@
-import _default = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new _default.PrismaClient();
+const prisma = new PrismaClient();
+
+export default prisma;
--- appBackend/src/index.ts
+++ appBackend/src/index.ts
@@ -1,1 +1,16 @@
-// ha
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server Running aman ");
+})
+
+app.listen(PORT, () => {
+  console.log("Server is running on port : ", PORT);
+});
+
+
+
+
