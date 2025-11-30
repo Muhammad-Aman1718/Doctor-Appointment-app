701bb3da05dd4f6acf6f1b648ce95227cf0b21ac
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-11-30 21:20:47 +0500

    an

--- appBackend/src/index.ts
+++ appBackend/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server Running aman ... ");
+  res.send("Server Running aman");
 })
 
 app.listen(PORT, () => {
