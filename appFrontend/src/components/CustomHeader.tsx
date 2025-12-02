c68695ce815fa7e14332df3612b53e473eae434d
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-02 20:55:29 +0500

    working on api

--- appFrontend/src/components/CustomHeader.tsx
+++ appFrontend/src/components/CustomHeader.tsx
@@ -119,7 +119,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useAppNavigation } from '../hooks/useAppNavigation';
-import Icon from 'react-native-vector-icons/Entypo';
+// import Icon from 'react-native-vector-icons/Entypo';
 
 interface CustomHeaderProps {
   title?: string;
@@ -131,12 +131,12 @@ const CustomHeader: React.FC<CustomHeaderProps> = ({ title }) => {
     <View style={styles.container}>
       <View style={styles.leftContainer}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon
+          {/* <Icon
             name="chevron-thin-left"
             size={24}
             color="#2260FF"
             style={styles.icon}
-          />
+          /> */}
         </TouchableOpacity>
       </View>
 
